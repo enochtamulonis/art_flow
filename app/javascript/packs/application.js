e3bcaa86e583a1f19ae1125b7d8c4707ae5aa4f0
Enoch Tamulonis <yungboiiindigo@gmail.com>
2020-11-14 16:06:25 -0500

    changed to webcrunch way

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,4 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import '../stylesheets/application.css'
+import "stylesheets/application" // ADD THIS LINE
