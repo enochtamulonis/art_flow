de5990362607d6ceab67cd3f5de54306cacf6661
Enoch Tamulonis <yungboiiindigo@gmail.com>
2020-11-14 14:05:45 -0500

    admin page andindex page styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,13 @@ class PagesController < ApplicationController
   before_action :is_admin?, only: [:admin]
   def home
     @newest_paintings = Painting.order(created_at: :desc).max(3)
-
   end
 
   def about
   end
 
   def admin
-
+    @paintings = Painting.order(updated_at: :desc)
   end
 
   private
