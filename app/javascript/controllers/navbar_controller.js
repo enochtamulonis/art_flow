baf1694dbd526c0a62a33f72b7b48ceaa425b802
Enoch Tamulonis <yungboiiindigo@gmail.com>
2021-01-30 19:58:02 -0500

    Added in mobile responsive navbar dropdown and redesigned home page image and buttons

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -5,4 +5,8 @@ export default class extends Controller {
   openCart(event) {
     document.getElementById("sidebar").classList.toggle("hidden");
   }
+
+  openNavDropdown() {
+    document.getElementById("navbar_dropdown").classList.toggle("hidden");
+  }
 }
